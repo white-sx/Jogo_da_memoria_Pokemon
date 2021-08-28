60c3c706e700704d29bb5e4250dec9491deedfd4
Rafael <faelsant10@gmail.com>
2021-08-28 11:45:52 -0300

    inserindo o javascript

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,31 +1,32 @@
 const FRONT = "card_front";
 const BACK = "card_back";
 
-let pokemons = ['bul',	'cat', 'cha', 'chi', 'eev', 'mun', 'pha', 'pike', 'san', 'squ']
+let pokemons = ['bul', 'cat', 'cha', 'chi', 'eev', 'mun', 'pha', 'pike', 'san', 'squ']
 
 creatCardsFromPokemons(pokemons);
 function creatCardsFromPokemons(pokemons) {
-				let cards = [];
-				
-				for(let pokemon of pokemons) {
-								cards.push(createPairFromPokemon(pokemon));
-				}
-				
-				console.log(cards.flatMap(pair => pair));
+  let cards = [];
+
+  for (let pokemon of pokemons) {
+    cards.push(createPairFromPokemon(pokemon));
+  }
+
+  console.log(cards.flatMap(pair => pair));
 }
 
 function createPairFromPokemon(pokemon) {
-				return [{
-								id: createIdWithPokemon(pokemon),
-								icon: pokemon,
-								flipped: false,
-				}, {
-								id: createIdWithPokemon(pokemon),
-								icon: pokemon,
-								flipped: false,
-				}]
+  return [{
+    id: createIdWithPokemon(pokemon),
+    icon: pokemon,
+    flipped: false,
+  },
+    {
+      id: createIdWithPokemon(pokemon),
+      icon: pokemon,
+      flipped: false,
+    }]
 }
 
 function createIdWithPokemon(pokemon) {
-				return pokemon + parseInt(Math.random() * 1000);
+  return pokemon + parseInt(Math.random() * 1000);
 }
