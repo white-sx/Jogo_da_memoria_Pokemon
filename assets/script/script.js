4d1ff8b2c21997b63954ac2e27c3492330cf9bd9
Rafael <faelsant10@gmail.com>
2021-08-30 13:49:21 -0300

    Montando o Tabuleiro

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,85 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
-let pokemons = ['bul', 'cat', 'cha', 'chi', 'eev', 'mun', 'pha', 'pike', 'san', 'squ']
+let pokemons = ['bul', 'cat', 'cha', 'chi', 'eev', 'mun', 'pha', 'pik', 'san', 'squ'];
+
+let cards = null;
+
+startGame();
+
+function startGame() {
+  cards = creatCardsFromPokemons(pokemons);
+  shuffleCards(cards);
+  initializeCards();
+
+}
+
+function initializeCards() {
+  let gameBoard = document.getElementById("gameboard");
+
+  cards.forEach((card) => {
+    cardElement = document.createElement('div');
+    cardElement.id = card.id;
+    cardElement.classList.add(CARD);
+    cardElement.dataset.icon = card.icon;
+
+    createCardContent(card, cardElement);
+
+    cardElement.addEventListener('click', flippiCard)
+    gameBoard.appendChild(cardElement);
+  })
+}
+
+function createCardContent(card, cardElement) {
+  createCardFace(FRONT, card, cardElement);
+  createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+  let cardElementFace = document.createElement('div');
+  cardElementFace.classList.add(face);
+
+
+  if (face === FRONT) {
+    let iconElement = document.createElement('img');
+    iconElement.src = "./assets/img/" + card.icon + ".png";
+    iconElement.classList.add(ICON);
+    cardElementFace.appendChild(iconElement);
+  } else {
+  		let iconElement = document.createElement('img');
+    iconElement.src = "./assets/img/poke.png";
+    iconElement.classList.add(ICON);
+    cardElementFace.appendChild(iconElement);
+  }
+  element.appendChild(cardElementFace);
+}
+
+function shuffleCards(cards) {
+  let currentIndex = cards.length;
+  let randomIndex = 0;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    [cards[randomIndex],
+      cards[currentIndex]] = [cards[currentIndex],
+      cards[randomIndex]]
+  }
+}
+
 
 creatCardsFromPokemons(pokemons);
 function creatCardsFromPokemons(pokemons) {
   let cards = [];
 
-  for (let pokemon of pokemons) {
+  pokemons.forEach((pokemon) => {
     cards.push(createPairFromPokemon(pokemon));
-  }
+  })
 
-  console.log(cards.flatMap(pair => pair));
+  return cards.flatMap(pair => pair);
 }
 
 function createPairFromPokemon(pokemon) {
@@ -29,4 +97,8 @@ function createPairFromPokemon(pokemon) {
 
 function createIdWithPokemon(pokemon) {
   return pokemon + parseInt(Math.random() * 1000);
+}
+
+function flippiCard() {
+  this.classList.add("flip");
 }
