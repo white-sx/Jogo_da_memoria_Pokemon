083909cf5bf06e582b4eb36e67ad732cd21d1164
Rafael <faelsant10@gmail.com>
2021-09-03 17:11:30 -0300

    erro que não consigo resolver

---
+++ assets/script/game.js
@@ -0,0 +1,92 @@
+let game = {
+
+  lockMode: false,
+  firstCard: null,
+  secondCard: null,
+
+  setCard: function (id) {
+  
+    let card = this.cards.filter(card => card.id === id)[0];
+			console.log(card);
+    if (card.flipped = true || this.lockMode) {
+      return false;
+    }
+
+    if (!this.firstCard) {
+      this.firstCard = card;
+      this.firstCard.flipped = true;
+      return true;
+    } else {
+      this.secondCard = card;
+      this.secondCard.flipped = true;
+      this.lockMode = true;
+      return true;
+    }
+  },
+
+
+  checkMatch: function () {
+    return this.firstCard.icon === this.secondCard.icon;
+  },
+
+  clearCard: function() {
+    this.firstCard = null;
+    this.secondCard = null;
+    this.lockMode = false;
+  },
+
+  pokemons: ['bul',
+    'cat',
+    'cha',
+    'chi',
+    'eev',
+    'mun',
+    'pha',
+    'pik',
+    'san',
+    'squ'],
+    
+  cards: null,
+  
+  creatCardsFromPokemons: function() {
+    this.cards = [];
+
+    this.pokemons.forEach((pokemon) => {
+      this.cards.push(this.createPairFromPokemon(pokemon));
+    })
+
+    this.cards = this.cards.flatMap(pair => pair);
+    this.shuffleCards();
+    return this.cards;
+  },
+  createPairFromPokemon: function(pokemon) {
+    return [{
+      id: this.createIdWithPokemon(pokemon),
+      icon: pokemon,
+      flipped: false,
+    },
+      {
+        id: this.createIdWithPokemon(pokemon),
+        icon: pokemon,
+        flipped: false,
+      }]
+  },
+  createIdWithPokemon: function(pokemon) {
+    return pokemon + parseInt(Math.random() * 1000);
+  },
+
+  shuffleCards: function(cards) {
+    let currentIndex = this.cards.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [this.cards[randomIndex],
+        this.cards[currentIndex]] = [this.cards[currentIndex],
+        this.cards[randomIndex]]
+    }
+  },
+
+}
--- assets/script/script.js
+++ assets/script/script.js
@@ -3,102 +3,76 @@ const BACK = "card_back";
 const CARD = "card";
 const ICON = "icon";
 
-let pokemons = ['bul', 'cat', 'cha', 'chi', 'eev', 'mun', 'pha', 'pik', 'san', 'squ'];
-
-let cards = null;
-
 startGame();
 
 function startGame() {
-  cards = creatCardsFromPokemons(pokemons);
-  shuffleCards(cards);
-  initializeCards();
+	initializeCards(game.creatCardsFromPokemons());
 
 }
 
 function initializeCards() {
-  let gameBoard = document.getElementById("gameboard");
+	let gameBoard = document.getElementById("gameboard");
 
-  cards.forEach((card) => {
-    cardElement = document.createElement('div');
-    cardElement.id = card.id;
-    cardElement.classList.add(CARD);
-    cardElement.dataset.icon = card.icon;
+	game.cards.forEach((card) => {
+		cardElement = document.createElement('div');
+		cardElement.id = card.id;
+		cardElement.classList.add(CARD);
+		cardElement.dataset.icon = card.icon;
 
-    createCardContent(card, cardElement);
+		createCardContent(card, cardElement);
 
-    cardElement.addEventListener('click', flippiCard)
-    gameBoard.appendChild(cardElement);
-  })
+		cardElement.addEventListener('click', flipCard)
+		gameBoard.appendChild(cardElement);
+	})
 }
 
 function createCardContent(card, cardElement) {
-  createCardFace(FRONT, card, cardElement);
-  createCardFace(BACK, card, cardElement);
+	createCardFace(FRONT, card, cardElement);
+	createCardFace(BACK, card, cardElement);
 }
 
 function createCardFace(face, card, element) {
-  let cardElementFace = document.createElement('div');
-  cardElementFace.classList.add(face);
+	let cardElementFace = document.createElement('div');
+	cardElementFace.classList.add(face);
 
 
-  if (face === FRONT) {
-    let iconElement = document.createElement('img');
-    iconElement.src = "./assets/img/" + card.icon + ".png";
-    iconElement.classList.add(ICON);
-    cardElementFace.appendChild(iconElement);
-  } else {
-  		let iconElement = document.createElement('img');
-    iconElement.src = "./assets/img/poke.png";
-    iconElement.classList.add(ICON);
-    cardElementFace.appendChild(iconElement);
-  }
-  element.appendChild(cardElementFace);
-}
-
-function shuffleCards(cards) {
-  let currentIndex = cards.length;
-  let randomIndex = 0;
-
-  while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    [cards[randomIndex],
-      cards[currentIndex]] = [cards[currentIndex],
-      cards[randomIndex]]
-  }
+	if (face === FRONT) {
+		let iconElement = document.createElement('img');
+		iconElement.src = "./assets/img/" + card.icon + ".png";
+		iconElement.classList.add(ICON);
+		cardElementFace.appendChild(iconElement);
+	} else {
+		let iconElement = document.createElement('img');
+		iconElement.src = "./assets/img/poke.png";
+		iconElement.classList.add(ICON);
+		cardElementFace.appendChild(iconElement);
+	}
+	element.appendChild(cardElementFace);
 }
 
 
-creatCardsFromPokemons(pokemons);
-function creatCardsFromPokemons(pokemons) {
-  let cards = [];
 
-  pokemons.forEach((pokemon) => {
-    cards.push(createPairFromPokemon(pokemon));
-  })
 
-  return cards.flatMap(pair => pair);
-}
+game.creatCardsFromPokemons(game.pokemons);
 
-function createPairFromPokemon(pokemon) {
-  return [{
-    id: createIdWithPokemon(pokemon),
-    icon: pokemon,
-    flipped: false,
-  },
-    {
-      id: createIdWithPokemon(pokemon),
-      icon: pokemon,
-      flipped: false,
-    }]
-}
 
-function createIdWithPokemon(pokemon) {
-  return pokemon + parseInt(Math.random() * 1000);
-}
+function flipCard() {
+	if (game.setCard(this.id)) {
 
-function flippiCard() {
-  this.classList.add("flip");
-}
+		this.classList.add("flip");
+		if (game.checkMatch()) {
+			game.clearCards();
+		} else {
+			setTimeout(() => {
+				let firstCardView = document.getElementById(game.firstCard.id);
+				let secondCardView = document.getElementById(game.secondCard.id);
+
+				firstCardView.classList.remove('flip');
+				secondCardView.classList.remove('flip');
+				game.clearCards();
+			}, 1000);
+		};
+	}
+
+
+};
